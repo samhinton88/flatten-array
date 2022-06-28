a582bf55eaeca6ee63cb954ad9667bd0baa9a80e
samhinton88 <sam.hinton88@gmail.com>
2022-06-28 21:53:06 +0100

    test: empty and bad input cases

--- flatten.test.js
+++ flatten.test.js
@@ -4,4 +4,12 @@ describe("flatten", () => {
   it("flattens nested arrays", () => {
     expect(flatten([1, [2, [3, [4, [5]]]]])).toEqual([1, 2, 3, 4, 5]);
   });
+
+  it("handles empty arrays", () => {
+    expect(flatten([])).toEqual([])
+  })
+
+  it("throws on non-aray input", () => {
+    expect(() => flatten("whups")).toThrow()
+  })
 });
