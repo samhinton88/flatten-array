f6a004c3148d3b1f8078cf3bbefae8c2cfbf952b
samhinton88 <sam.hinton88@gmail.com>
2022-06-29 10:09:28 +0100

    chore: format tests

--- flatten.test.js
+++ flatten.test.js
@@ -6,29 +6,37 @@ describe("flatten", () => {
   });
 
   it("handles empty arrays", () => {
-    expect(flatten([])).toEqual([])
-  })
+    expect(flatten([])).toEqual([]);
+  });
 
   it("handles nested empty arrays", () => {
-    expect(flatten([[[[[[[[[[[[[[]]]]]]]]]]][[[[[[[]]]]]]]]]])).toEqual([])
-  })
+    expect(flatten([[[[[[[[[[[[[[]]]]]]]]]]][[[[[[[]]]]]]]]]])).toEqual([]);
+  });
 
   it("ignores further arguments", () => {
-    expect(flatten([1, [2, [3, [4, [5]]]]], { some: 'garbage' })).toEqual([1, 2, 3, 4, 5]);
+    expect(flatten([1, [2, [3, [4, [5]]]]], { some: "garbage" })).toEqual([
+      1, 2, 3, 4, 5,
+    ]);
   });
 
   it("throws on non-aray input", () => {
-    expect(() => flatten("whups")).toThrow()
-    expect(() => flatten(null)).toThrow()
-    expect(() => flatten(1)).toThrow()
-    expect(() => flatten(true)).toThrow()
-  })
+    expect(() => flatten("whups")).toThrow();
+    expect(() => flatten(null)).toThrow();
+    expect(() => flatten(1)).toThrow();
+    expect(() => flatten(true)).toThrow();
+  });
 
   it("throws on empty input", () => {
-    expect(() => flatten()).toThrow()
-  })
+    expect(() => flatten()).toThrow();
+  });
 
   it("allows elements of mixed type", () => {
-    expect(flatten([1, ['2', [false, [{}, [null]]]]])).toEqual([1, '2', false, {}, null]);
-  })
+    expect(flatten([1, ["2", [false, [{}, [null]]]]])).toEqual([
+      1,
+      "2",
+      false,
+      {},
+      null,
+    ]);
+  });
 });
